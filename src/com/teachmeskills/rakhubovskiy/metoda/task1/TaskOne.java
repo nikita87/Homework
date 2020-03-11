b00cd58d10d2f296526215f67fe60908afc49407
nikita87 <nikitarakhubovskiy@gmail.com>
2020-03-11 22:09:28 +0300

    delete metoda

--- src/com/teachmeskills/rakhubovskiy/metoda/task1/TaskOne.java
+++
@@ -1,8 +0,0 @@
-package com.teachmeskills.rakhubovskiy.metoda.task1;
-
-public class TaskOne {
-    public static void main(String[] args)
-    {
-        System.out.println("Hello, world!");
-    }
-}
