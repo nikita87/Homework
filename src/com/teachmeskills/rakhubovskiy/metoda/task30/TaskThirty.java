1e6cd178c5f68ab7d0ef97458da504b8ff25af10
nikita87 <nikitarakhubovskiy@gmail.com>
2020-03-24 23:25:02 +0300

    add little fixes to class TaskThirty

--- src/com/teachmeskills/rakhubovskiy/metoda/task30/TaskThirty.java
+++ src/com/teachmeskills/rakhubovskiy/metoda/task30/TaskThirty.java
@@ -1,8 +1,6 @@
 package  com.teachmeskills.rakhubovskiy.metoda.task30;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 /*Создать коллекцию, наполнить ее случайными числами. Удалить повторяющиеся
 числа.*/
@@ -16,18 +14,15 @@ public class TaskThirty {
         List<Integer> numberList = new ArrayList<>();
         for (int i = 0; i < sizeList; i++){
             int numberGen = (int) (Math.random()*20);
-            numberList.add(i, numberGen);
+            numberList.add(numberGen);
         }
         System.out.println("Исходный список = " + numberList.toString());
+
         //удаляем все дубликаты из списка
+        Set<Integer> numberSet = new HashSet<>();
         for (int i = 0; i < numberList.size(); i++){
-            for (int j = i + 1; j < numberList.size(); j++){
-                if(numberList.get(i) == numberList.get(j)){
-                    numberList.remove(j);
-                    j--;
-                }
-            }
+            numberSet.add(numberList.get(i));
         }
-        System.out.println("Список с уникальными числами = " + numberList.toString());
+        System.out.println("Список с уникальными числами = " + numberSet.toString());
     }
 }
