fbd3d255e805f8c8b1058c9a9a0714e252c129e1
nikita87 <nikitarakhubovskiy@gmail.com>
2020-03-31 22:20:09 +0300

    add lesson12

---
+++ src/com/teachmeskills/rakhubovskiy/metoda/lesson12/doptask2/TextFormatter.java
@@ -0,0 +1,30 @@
+package com.teachmeskills.rakhubovskiy.metoda.lesson12.doptask2;
+
+public class TextFormatter {
+
+    public static int getCountWordsLine(String str){
+        str = str.replaceAll("[^a-zA-Zа-яА-Я]", " ");
+        int words = str.split("\\s+").length;
+        return words;
+    }
+
+    public static boolean getLineWithPalindrom(String str) {
+        str = str.replaceAll("[^a-zA-Zа-яА-Я]", " ");
+        String[] words = str.trim().split("\\s+");
+        for (int i = 0; i < words.length; i++) {
+            int count = 0;
+            char[] letterInWord = words[i].toCharArray();
+            for (int j = 0; j < letterInWord.length / 2; j++) {
+                if (letterInWord[j] != letterInWord[letterInWord.length - j - 1]) {
+                    break;
+                }
+                count++;
+            }
+            if (count == letterInWord.length / 2) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
---
+++ src/com/teachmeskills/rakhubovskiy/metoda/lesson12/doptask3/DopTaskThree.java
@@ -0,0 +1,81 @@
+package com.teachmeskills.rakhubovskiy.metoda.lesson12.doptask3;
+
+/*Проверка на цензуру:
+Создаете 2 файла.
+1 - й. Содержит исходный текст.
+2 - й. Содержит слова недопустимые в тексте(black list). Структура файла
+определите сами, хотите каждое слово на новой строке, хотите через запятую
+разделяйте, ваша программа делайте как считаете нужным.
+    Задача: необходимо проверить проходит ли текст цензуру. Если в тексте не
+встретилось ни одного недопустимого слова, то выводите сообщение о том что
+текст прошёл проверку на цензуру. Если нет, то выводите соответствуюущее
+сообщение, кол-во предложений не прошедших проверку и сами предложения
+подлежащие исправлению.
+*/
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class DopTaskThree {
+    public static void main(String[] args) throws IOException {
+
+        FileReader fileReaderText, fileReaderBlackList;
+
+        try {
+            fileReaderText = new FileReader("fileDopTask3.txt");
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+            return;
+        }
+
+        BufferedReader brText = new BufferedReader(fileReaderText);
+
+        String lineText, lineBlackList;
+        int countBlackListLines = 0;
+
+        while ((lineText = brText.readLine())!= null){
+
+            boolean isBlackListLine = false;
+
+            String line = lineText.replaceAll("[^a-zA-Z]", " ");
+            String[] words = line.trim().split("\\s+");
+
+            for (int i = 0; i < words.length; i++){
+
+                try {
+                    fileReaderBlackList = new FileReader(("fileDopTask3_blacklist.txt"));
+                } catch (FileNotFoundException e) {
+                    System.out.println("Blacklist not found");
+                    return;
+                }
+
+                BufferedReader brBlackList = new BufferedReader(fileReaderBlackList);
+
+                while ((lineBlackList = brBlackList.readLine()) != null){
+                    if(words[i].equals(lineBlackList)){
+                        countBlackListLines++;
+                        isBlackListLine = true;
+                        break;
+                    }
+                }
+                fileReaderBlackList.close();
+                if (isBlackListLine == true){ //если хотя бы одно слово попадает в blacklist, то сразу выходим из цикла for
+                    break;
+                }
+            }
+            if (isBlackListLine == true){
+                System.out.println(lineText);
+            }
+        }
+        if (countBlackListLines > 0){
+            System.out.println("\n" + "The text was not censored!");
+            System.out.println("Number of uncensored offers: " + countBlackListLines);
+        } else {
+            System.out.println("Success! The text censored!");
+        }
+        fileReaderText.close();
+    }
+}
+
